f38ea21661f21356446eb2b107f69d022121cd29
Markus Sintonen <markus.sintonen@gmail.com>
2025-12-26 18:52:06 +0200

    Improve BytesMut capacity. Use double buffer for read

--- src/response/internal/body_reader.rs
+++ src/response/internal/body_reader.rs
@@ -45,11 +45,12 @@ impl BodyReader {
         } else {
             assert!(!has_more, "Should have fully consumed the response");
         }
+
         if body_receiver.is_none()
             && let Some(a) = request_semaphore_permit.take()
         {
-            drop(a)
-        } // No more body to read, release the semaphore permit
+            drop(a) // No more body to read, release the semaphore permit
+        }
 
         let body_reader = BodyReader {
             body_receiver,
@@ -62,39 +63,35 @@ impl BodyReader {
         Ok((body_reader, head))
     }
 
-    pub async fn next_chunk(&mut self, cancel: &mut CancelHandle) -> PyResult<Option<Bytes>> {
-        async fn next(this: &mut BodyReader, cancel: &mut CancelHandle) -> PyResult<Option<Bytes>> {
-            if let Some(chunk) = this.chunks.pop_front() {
-                return Ok(Some(chunk));
-            }
-
-            let Some(body_rx) = this.body_receiver.as_mut() else {
-                return Ok(None); // No body receiver, fully consumed
-            };
-
-            let chunks_buffer = tokio::select! {
-                res = body_rx.recv() => res,
-                _ = cancel.cancelled().fuse() => Err(CancelledError::new_err("Read was cancelled")),
-            };
-            let Some(chunks_buffer) = chunks_buffer? else {
-                return Ok(None); // No more data
-            };
-
-            let mut buffer_iter = chunks_buffer.into_iter();
-            let first_chunk = buffer_iter.next();
-            for rest_chunk in buffer_iter {
-                this.chunks.push_back(rest_chunk);
-            }
-            Ok(first_chunk)
+    async fn read_next(&mut self, cancel: &mut CancelHandle) -> PyResult<Option<Bytes>> {
+        if let Some(chunk) = self.chunks.pop_front() {
+            return Ok(Some(chunk));
         }
 
-        match next(self, cancel).await {
-            Ok(Some(chunk)) => {
-                self.read_bytes += chunk.len();
-                Ok(Some(chunk))
-            }
-            Ok(None) => Ok(None),
-            Err(err) => Err(err),
+        let Some(body_rx) = self.body_receiver.as_mut() else {
+            return Ok(None); // No body receiver, fully consumed
+        };
+
+        let chunks_buffer = tokio::select! {
+            res = body_rx.recv() => res,
+            _ = cancel.cancelled().fuse() => Err(CancelledError::new_err("Read was cancelled")),
+        };
+        let Some(chunks_buffer) = chunks_buffer? else {
+            return Ok(None); // No more data
+        };
+
+        let mut buffer_iter = chunks_buffer.into_iter();
+        let first_chunk = buffer_iter.next();
+        self.chunks.extend(buffer_iter);
+        Ok(first_chunk)
+    }
+
+    pub async fn next_chunk(&mut self, cancel: &mut CancelHandle) -> PyResult<Option<Bytes>> {
+        if let Some(chunk) = self.read_next(cancel).await? {
+            self.read_bytes += chunk.len();
+            Ok(Some(chunk))
+        } else {
+            Ok(None)
         }
     }
 
@@ -112,11 +109,12 @@ impl BodyReader {
             None => BytesMut::new(),
         };
 
-        while let Some(chunk) = self.next_chunk(cancel).await? {
+        while let Some(chunk) = self.read_next(cancel).await? {
             bytes.extend_from_slice(&chunk);
         }
 
         let bytes = bytes.freeze();
+        self.read_bytes = bytes.len();
         self.fully_consumed_body = Some(bytes.clone()); // Zero-copy clone
         Ok(bytes)
     }
@@ -135,7 +133,7 @@ impl BodyReader {
         let mut remaining = amount;
 
         while remaining > 0 {
-            if let Some(mut chunk) = self.next_chunk(cancel).await? {
+            if let Some(mut chunk) = self.read_next(cancel).await? {
                 if chunk.len() > remaining {
                     let extra = chunk.split_off(remaining);
                     self.chunks.push_front(extra);
@@ -150,6 +148,7 @@ impl BodyReader {
         if collected.is_empty() {
             return Ok(None);
         }
+        self.read_bytes += collected.len();
         Ok(Some(collected.freeze()))
     }
 
@@ -230,7 +229,7 @@ impl Reader {
         buffer_size: usize,
         runtime: RuntimeHandle,
     ) -> Receiver {
-        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        let (tx, rx) = tokio::sync::mpsc::channel(2);
         let close_token = CancellationToken::new();
         let close_token_child = close_token.child_token();
 
